49db3fa0713823c2c7f226c167e4732632b96e11
Emmanuel Obonyo <emmyobonyo@gmail.com>
2023-01-10 10:07:37 +0300

    Some stuff

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,14 +12,31 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         if @article.save
         # redirect to articles show page
+            flash[:notice] = "Alert was created successfully."
             redirect_to @article
         # render @article, status: :unprocessable_entity
         else
             render 'new', status: :unprocessable_entity
+        end
+    end
+
+    def update
+        # Find article that needs to be edited
+        @article = Article.find(params[:id])
+        #  update the article. We need to white list and make title, and descriptin available to use
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = 'Article was updated successfully.'
+            redirect_to @article
+        else
+            render 'edit', status: :unprocessable_entity
         end
     end
 end
