6933f4b3e3e6cefc1360276852da23cd7965be42
Emmanuel Obonyo <emmyobonyo@gmail.com>
2023-01-20 12:12:03 +0300

    Allow a user to delete an artic;e

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, only: [:edit, :update, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def new
     @user = User.new
@@ -38,6 +38,13 @@ class UsersController < ApplicationController
       render 'new', status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and articles deleted"
+    redirect_to articles_path
+  end 
 
   private 
 
