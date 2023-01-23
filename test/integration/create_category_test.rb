242b44111b17257866dbba25e35189866597c081
Emmanuel Obonyo <emmyobonyo@gmail.com>
2023-01-23 16:55:42 +0300

    Add integration tests to error messages

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -8,7 +8,19 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
       post categories_path, params: { category: { name: "Sports"} }
       assert_response :redirect
      end
-     follow :redirect!
+     follow_redirect!
      assert_response :success
+     assert_match "Sports", response.body
   end
+
+  test "get new category form and reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference "Category.count" do
+     post categories_path, params: { category: { name: " "} }
+    end
+    assert_match "errors", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+ end
 end
