3f99fab068aad642bb90db09176362093cbbcf24
Emmanuel Obonyo <emmyobonyo@gmail.com>
2023-01-23 20:03:51 +0300

    Allow only admins to create categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,6 +1,7 @@
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
     setup do 
         @category = Category.create(name: "Sports")
+        @admin_user = User.create(username: 'johndoe', email: 'johndoe@gmail.com', password: 'password', admin: true)
     end
 
     test "should get index" do 
@@ -14,11 +15,18 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     end
 
     test "should create category" do 
-        assert_difference("Category.count", 1) do
+        sign_in_as(@admin_user)
+        assert_difference('Category.count', 1) do 
+            post categories_url, params: { category: { name: "Travel" } }
+        end
+    end
+    
+    test "should not create category if not admin" do 
+        assert_no_difference("Category.count") do
           post categories_url, params: { category: { name: "Travel" } }
         end
 
-        assert_redirected_to category_url(Category.last)
+        assert_redirected_to categories_url
     end
 
     test "Should show category" do
