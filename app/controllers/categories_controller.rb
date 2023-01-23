5f3d058122e2ebb29e57859f1888b60e77b13ee9
Emmanuel Obonyo <emmyobonyo@gmail.com>
2023-01-23 16:29:15 +0300

    Add integration tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,28 @@
 class CategoriesController < ApplicationController
     def new 
+        @category = Category.new
+    end
+
+    def create 
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "Category was successfuly created"
+            redirect_to @category
+        else
+            render 'new', status: :unprocessable_entity
+        end 
     end
 
     def show 
+        @category = Category.find(params[:id])
     end
 
-    def index 
+    def index
+        
+    end
+
+    private 
+    def category_params
+        params.require(:category).permit(:name)
     end
 end
